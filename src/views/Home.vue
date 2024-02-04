5d94a9c769dee8df190e598f778dd84fb4ebf48a
Emerson Solano <emerson-solano@hotmail.com>
2024-02-03 22:43:44 -0600

    Feature: agregando enrutamiento de portafolio

---
+++ src/views/Home.vue
@@ -0,0 +1,14 @@
+<script setup>
+    //
+</script>
+
+<template>
+    <div class="flex flex-col items-center justify-center min-h-screen py-2">
+        <h1 class="text-2xl font-bold text-blue-600">Welcome to Home Page</h1>
+        <p class="mt-2 text-gray-600">This is the home page of my application.</p>
+    </div>
+</template>
+
+<style lang="css" scoped>
+
+</style>
