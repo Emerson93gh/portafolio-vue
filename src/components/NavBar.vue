84869dfdba9ce5460ff3cc3d070143afb6e67bbf
Emerson Solano <emerson-solano@hotmail.com>
2024-02-05 21:49:09 -0600

    Feature: agregando menu para movil

--- src/components/NavBar.vue
+++ src/components/NavBar.vue
@@ -1,4 +1,5 @@
 <script setup>
+import { ref } from 'vue';
 import { useRouter, useRoute } from 'vue-router'
 
 const router = useRouter()
@@ -18,6 +19,7 @@ const navigation = [
   { name: 'Repositorios', href: '/repositorios', current: false },
 ]
 
+const isOpen = ref(false)
 const isCurrent = (href) => route.path === href
 </script>
 
@@ -43,7 +45,31 @@ const isCurrent = (href) => route.path === href
         </div>
         <div class="-mr-2 flex md:hidden">
           <!-- Mobile menu button -->
+          <button @click="isOpen = !isOpen" class="bg-gray-800 inline-flex items-center justify-center p-2 rounded-md text-gray-400 hover:text-white hover:bg-gray-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-gray-800 focus:ring-white">
+            <span class="sr-only">Open main menu</span>
+            <!-- Icon when menu is closed. -->
+            <svg v-if="!isOpen" class="block h-6 w-6" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor" aria-hidden="true">
+              <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4 6h16M4 12h16M4 18h16" />
+            </svg>
+            <!-- Icon when menu is open. -->
+            <svg v-else class="block h-6 w-6" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor" aria-hidden="true">
+              <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M6 18L18 6M6 6l12 12" />
+            </svg>
+          </button>
         </div>
+      </div>
+    </div>
+
+    <!-- Mobile menu, show/hide based on menu state. -->
+    <div v-if="isOpen" class="md:hidden">
+      <div class="px-2 pt-2 pb-3 space-y-1 sm:px-3">
+        <ul>
+          <li v-for="item in navigation" :key="item.name">
+            <a @click="router.push(item.href)" :class="{'text-white': isCurrent(item.href), 'text-gray-300 hover:text-white': !isCurrent(item.href)}">
+              {{ item.name }}
+            </a>
+          </li>
+        </ul>
       </div>
     </div>
   </nav>
